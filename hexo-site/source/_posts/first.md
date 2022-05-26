00795254db4c12c682232895b925db3be49ac740
xiaohaoMT <1037432782@qq.com>
2022-05-26 11:01:21 +0800

    update 2022年 05月 26日 星期四 11:01:21 CST

--- hexo-site/source/_posts/first.md
+++
@@ -1,6 +0,0 @@
----
-title: first
-date: 2022-05-26 10:44:25
-tags:
----
-vscode test
--- hexo-site/update.sh
+++
@@ -1,7 +0,0 @@
-hexo g
-cp -r public/* ..
-cd ..
-git add .
-git commit -m "update $(date)"
-git push origin main
-               
